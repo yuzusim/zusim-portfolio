25b25475f209fe7ed06a936dd92099b7a1fdf7ef
yuzusim <yuzusim@gmail.com>
2026-06-26 19:09:47 +0900

    test

--- src/pages/project/mockup/mobile/Showcase.js
+++ src/pages/project/mockup/mobile/Showcase.js
@@ -1,104 +1,13 @@
-// import styles from "../../ProjectDetail.module.scss";
-// import { getBackgroundStyle } from "utils/getBackgroundStyle";
-
-// function Showcase({ project }) {
-//   const showcase = project?.mockup?.mobile?.showcase;
-//   console.log("Showcase 렌더링");
-
-//   if (!showcase) return null;
-
-//   const { images = [], background, fade } = showcase;
-//   const [loaded, setLoaded] = useState(false);
-
-//   useEffect(() => {
-//     const list = images.map((img) => {
-//       return new Promise((resolve) => {
-//         const image = new Image();
-//         image.src = img.src;
-//         image.onload = resolve;
-//       });
-//     });
-
-//     Promise.all(list).then(() => setLoaded(true));
-//   }, [images]);
-
-//   return (
-//     <section
-//       className={styles.mobileShowcase}
-//       style={getBackgroundStyle(background)}
-//     >
-//       <div
-//         className={styles.ShowcaseInner}
-//         style={{
-//           "--fade-color": fade?.color || "#fff",
-//           "--fade-height": fade?.height || "50px",
-//         }}
-//       >
-//         {[1, 2, 3].map((column) => (
-//           // <div
-//           //   key={column}
-//           //   className={`${styles.gridSlider} ${
-//           //     column % 2 ? styles.columnUp : styles.columnDown
-//           //   }`}
-//           // >
-
-//           <div
-//             className={`${styles.gridSlider} ${
-//               loaded ? (column % 2 ? styles.columnUp : styles.columnDown) : ""
-//             }`}
-//           >
-//             {images
-//               .filter((img) => img.column === column)
-//               .concat(images.filter((img) => img.column === column))
-//               .map((img, idx) => (
-//                 <div key={idx} className={styles.card}>
-//                   <img
-//                     src={img.src}
-//                     className={`${styles.mockup} ${styles[img.size]}`}
-//                     alt=""
-//                     loading="eager"
-//                     decoding="sync"
-//                     fetchPriority="high"
-//                   />
-//                 </div>
-//               ))}
-//           </div>
-//         ))}
-//       </div>
-//     </section>
-//   );
-// }
-// export default Showcase;
-
-import { useEffect, useState } from "react";
 import styles from "../../ProjectDetail.module.scss";
 import { getBackgroundStyle } from "utils/getBackgroundStyle";
 
 function Showcase({ project }) {
   const showcase = project?.mockup?.mobile?.showcase;
-
-  const [loaded, setLoaded] = useState(false);
-
-  const { images = [], background, fade } = showcase;
-
-  useEffect(() => {
-    const promises = images.map((img) => {
-      return new Promise((resolve) => {
-        const image = new Image();
-
-        image.onload = resolve;
-        image.onerror = resolve;
-
-        image.src = img.src;
-      });
-    });
-
-    Promise.all(promises).then(() => {
-      setLoaded(true);
-    });
-  }, [images]);
+  console.log("Showcase 렌더링");
 
   if (!showcase) return null;
+
+  const { images = [], background, fade } = showcase;
 
   return (
     <section
@@ -116,7 +25,7 @@ function Showcase({ project }) {
           <div
             key={column}
             className={`${styles.gridSlider} ${
-              loaded ? (column % 2 ? styles.columnUp : styles.columnDown) : ""
+              column % 2 ? styles.columnUp : styles.columnDown
             }`}
           >
             {images
@@ -128,8 +37,6 @@ function Showcase({ project }) {
                     src={img.src}
                     className={`${styles.mockup} ${styles[img.size]}`}
                     alt=""
-                    loading="eager"
-                    decoding="sync"
                   />
                 </div>
               ))}
@@ -138,6 +45,33 @@ function Showcase({ project }) {
       </div>
     </section>
   );
-}
 
+  // return (
+  //   <section className={styles.mobileShowcase}>
+  //     <div className={styles.ShowcaseInner}>
+  //       {[1, 2, 3].map((column) => (
+  //         <div
+  //           key={column}
+  //           className={`${styles.gridSlider} ${
+  //             column % 2 ? styles.columnUp : styles.columnDown
+  //           }`}
+  //         >
+  //           {images
+  //             .filter((img) => img.column === column)
+  //             .concat(images.filter((img) => img.column === column))
+  //             .map((img, idx) => (
+  //               <div key={idx} className={styles.card}>
+  //                 <img
+  //                   src={img.src}
+  //                   className={`${styles.mockup} ${styles[img.size]}`}
+  //                   alt=""
+  //                 />
+  //               </div>
+  //             ))}
+  //         </div>
+  //       ))}
+  //     </div>
+  //   </section>
+  // );
+}
 export default Showcase;
